7acd5309f11aeb135ac5a5ae9ced8f03bfa38335
Ganguly <492476@cognizant.com>
2018-08-01 17:45:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  oddNumbers: Array<number> = [];
+  evenNumbers: Array<number> = [];
+
+
+  onStartClicked(number) {
+    number % 2 === 0 ? this.evenNumbers.push(number) : this.oddNumbers.push(number);
+    console.log(this.evenNumbers);
+  }
 }
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  interval;
+  randomNumer = 1;
+  @Output('startClicked') GenerateNumber = new EventEmitter<Number>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  startGame() {
+    this.interval = setInterval(() => {
+      this.GenerateNumber.emit(this.randomNumer);
+      this.randomNumer++;
+    }, 1000);
+  }
+
+  stopGame() {
+    clearInterval(this.interval);
+  }
+
+}
